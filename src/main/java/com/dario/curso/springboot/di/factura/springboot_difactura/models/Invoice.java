82858563f0f0ed598d394426269d83ce46e743c9
drox13 <dario.baron@uptc.edu.co>
2024-11-22 16:33:30 -0500

    67. Factura y relaciones entre sus componentes

---
+++ src/main/java/com/dario/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -0,0 +1,31 @@
+package com.dario.curso.springboot.di.factura.springboot_difactura.models;
+
+import java.util.List;
+
+public class Invoice {
+    private Client client;
+    private String description;
+    private List<Item> items;
+    
+    public Client getClient() {
+        return client;
+    }
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Item> getItems() {
+        return items;
+    }
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    
+
+}
