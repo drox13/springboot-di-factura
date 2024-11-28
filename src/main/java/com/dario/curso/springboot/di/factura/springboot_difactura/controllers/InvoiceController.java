b31ded376c80750d95baaaf7339bd487b60a63fa
drox13 <dario.baron@uptc.edu.co>
2024-11-28 10:11:28 -0500

    76. Contextos (Scope) de los componentes parte 2

--- src/main/java/com/dario/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/dario/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -11,20 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 @RequestMapping("/invoices")
 public class InvoiceController {
-
-    @Autowired
+     @Autowired
     private Invoice invoice;
     
     @GetMapping("/show")
     public Invoice show(){
         Invoice i = new Invoice();
         Client c = new Client();
-        c.setName(invoice.getClient().getName());
-        c.setLastname(invoice.getClient().getLastname());
-        i.setClient(c);
+        c.setName(i.getClient().getName());
+        c.setLastname(i.getClient().getLastname());
+        i.setClient(invoice.getClient());
         i.setDescription(invoice.getDescription());
         i.setItems(invoice.getItems());
-
         return i;
     }
 
